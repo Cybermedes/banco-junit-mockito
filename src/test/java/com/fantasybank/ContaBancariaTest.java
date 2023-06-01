69c0ebb2aec7722fefc79ddd543724402ec1c8b8
Lucas Paiva <lpaivamatiazzi@gmail.com>
2023-05-31 21:27:01 -0300

    Adiciona nomes customizados

--- src/test/java/com/fantasybank/ContaBancariaTest.java
+++ src/test/java/com/fantasybank/ContaBancariaTest.java
@@ -1,13 +1,17 @@
 package com.fantasybank;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 
+@DisplayName("Teste da classe ContaBancaria")
 public class ContaBancariaTest {
 
     //Teste para checar o metodo sacar()
     @Test
+    @DisplayName("Saque de 500 reais com sucesso.")
     public void testSacar() {
         ContaBancaria conta = new ContaBancaria(500, -1000);
         conta.sacar(300);
@@ -16,6 +20,7 @@ public class ContaBancariaTest {
 
     //Teste para checar o metodo depositar()
     @Test
+    @DisplayName("Deposito de 400 reais com sucesso.")
     public void testDepositar() {
         ContaBancaria conta = new ContaBancaria(400, 0);
         conta.depositar(500);
